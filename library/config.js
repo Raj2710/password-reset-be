3e3287937ecd0d8bbc7a7d37f96804fece3d8f81
Raj2710 <nagarajansai2727@gmail.com>
2021-07-07 13:24:15 +0530

    Configuration Updated

--- library/config.js
+++ library/config.js
@@ -3,5 +3,5 @@ const pwd = "raj2710@g"
 const CLIENT_ID="1094693455304-qlqoq90kafh134du91npemev5lfcqvk0.apps.googleusercontent.com"
 const CLIENT_SECRET="seMIWuJe1H2MMU0ygnQaMM85"
 const REDIRECT_URI="https://developers.google.com/oauthplayground"
-const REFRESH_TOKEN="1//04L42O784prHQCgYIARAAGAQSNwF-L9IrG6cykeVr-yUBpGx7nbu5lZXmBzqp2W7ylTmiiXiC5icPIb8OShtiLSk7s2ECyntBZ_w"
+const REFRESH_TOKEN="1//04-9eDhhK-XN5CgYIARAAGAQSNwF-L9Irq973wxcsGFzCUNVYmEMQmS5n37-XnRauVQHDROiz3rTJfEArxx8OceMZhPbZoJTrlWQ"
 module.exports={sender,pwd,CLIENT_ID,CLIENT_SECRET,REDIRECT_URI,REFRESH_TOKEN};
